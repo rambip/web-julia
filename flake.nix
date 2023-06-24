30fa7774a73a8a221e0962db7b164e3e5bacdd45
rambip <apero1808@gmail.com>
2023-06-24 15:23:12 +0200

    updated wasm-tooling deps

--- flake.nix
+++ flake.nix
@@ -1,22 +1,21 @@
 {
     description = "webapp to generate julia fractal";
-    inputs.wasm-tooling.url = github:rambip/wasm-tooling;
-
-    outputs = {self, nixpkgs, wasm-tooling}: {
-        defaultPackage.x86_64-linux = 
-            let pkgs = import nixpkgs {system = "x86_64-linux";};
-                tooling = pkgs.callPackage wasm-tooling {};
-            in
-            tooling.rust.buildWithTrunk {
-                src = ./.;
-                extraFiles = ["index.html" "index.js" "style.css"];
-            };
-        devShell.x86_64-linux = 
-            let pkgs = import nixpkgs {system = "x86_64-linux";};
-                tooling = pkgs.callPackage wasm-tooling {};
-            in
-            tooling.rust.devShell {
-                src = ./.;
-            } ;
+    inputs = {
+        wasm-tooling.url = github:rambip/wasm-tooling;
+        flake-utils.url = github:numtide/flake-utils;
     };
+
+    outputs = {self, nixpkgs, flake-utils, wasm-tooling}: with flake-utils.lib;
+        eachSystem [system.x86_64-linux system.x86_64-darwin] (system:
+            let rust-tooling = wasm-tooling.lib."${system}".rust;
+            in
+            {
+                packages.default =
+                    rust-tooling.buildWithTrunk {
+                        src = ./.;
+                        extraFiles = ["index.html" "index.js" "style.css"];
+                    };
+                devShells.default = rust-tooling.makeDevShell {src=./.;};
+            }
+        );
 }
